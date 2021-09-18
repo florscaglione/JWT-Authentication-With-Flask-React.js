b302cad65926e8f8d3d41ba58553fb913a723baf
florscaglione <florencia.scaglione@hotmail.com>
2021-09-18 01:15:14 +0000

    logout

--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -1,5 +1,6 @@
 import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useHistory } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/login.scss";
 
@@ -7,41 +8,20 @@ export const Login = () => {
 	const { store, actions } = useContext(Context);
 	const [email, setEmail] = useState(""); //hago estos 2 useStates porque necesito que el email y contraseña
 	const [password, setPassword] = useState(""); //queden almacenados (seteados) en una vble. para poderlos enviar al backend
-	const token = localStorage.getItem("token");
+	const history = useHistory();
 
-	console.log("This is your token", token);
-
+	console.log("This is your token", store.token);
 	const handleClick = () => {
-		const options = {
-			method: "POST",
-			headers: {
-				"Content-type": "application/json"
-			},
-			body: JSON.stringify({
-				email: email,
-				password: password
-			})
-		};
-
-		fetch("https://3001-purple-impala-p24iuy49.ws-eu16.gitpod.io/api/token", options)
-			.then(resp => {
-				if (resp.status === 200) return resp.json();
-				else alert("There has been some error");
-			})
-			.then(data => {
-				console.log("This came from the backend", data);
-				localStorage.setItem("token", data.access_token); //access_token es lo que me respondió el token en Postman
-			})
-			.catch(error => {
-				console.error("There was an error!!", error);
-			});
+		actions.login(email, password).then(() => {
+			history.push("/"); //redireccionamiento a la pág. principal
+		});
 	};
 
 	return (
 		<div className="text-center mt-5">
 			<h1>Login</h1>
-			{token && token != "" && token != undefined ? ( //condición TERNARIA (es un if/else "simplificado")
-				"You are logged in whit this token" + token
+			{store.token && store.token != "" && store.token != undefined ? ( //condición TERNARIA (es un if/else "simplificado")
+				"You are logged in whit this token" + store.token
 			) : (
 				<div>
 					<input
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -21,6 +21,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 				getActions().changeColor(0, "green");
 			},
 
+			syncTokenFromLocalStore: () => {
+				//esta funcion obtendrá el token del localStore y lo pondrá en el store (con el setStore)
+				const token = localStorage.getItem("token"); //pero siempre y cuando se cumplan estas condiciones del if
+				if (token && token != "" && token != undefined) setStore({ token: token });
+			},
+
+			logout: () => {
+				localStorage.removeItem("token"); //borro el token del localStorage
+				console.log("Login out");
+				setStore({ token: null }); //y establezco el token del store vacío
+			},
+
 			login: async (email, password) => {
 				//lo hacemos asíncrono para que sea más fácil de administrar
 				const options = {
@@ -46,6 +58,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 					const data = await resp.json();
 					console.log("This came from the backend", data);
 					localStorage.setItem("token", data.access_token); //access_token es lo que me respondió el token en Postman
+					setStore({ token: data.access_token });
 					return true;
 				} catch (error) {
 					console.log("There has been an error login in");
